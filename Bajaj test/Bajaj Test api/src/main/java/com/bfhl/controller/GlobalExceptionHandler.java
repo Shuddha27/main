070de3cc471014ee22a7f2aa6ce7ca9ed6e8a549
Shuddha27 <kaushikshuddha27@gmail.com>
2026-05-26 16:36:36 +0530

    GlobalExceptionHandler.java

--- Bajaj test/Bajaj Test api/src/main/java/com/bfhl/controller/GlobalExceptionHandler.java
+++ Bajaj test/Bajaj Test api/src/main/java/com/bfhl/controller/GlobalExceptionHandler.java
@@ -14,9 +14,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<BfhlResponse> handle(Exception e) {
         BfhlResponse r = new BfhlResponse();
         r.setSuccess(false);
-        r.setUserId("shubhda_agrawal_29112005");
-        r.setEmail("shubhdaagrawal230366@acropolis.in");
-        r.setRollNumber("0827cy231068");
+        r.setUserId("shuddhakaushik_27092005");
+        r.setEmail("shuddhakaushik230666@acropolis.in");
+        r.setRollNumber("0827CY231069");
         r.setOddNumbers(Collections.emptyList());
         r.setEvenNumbers(Collections.emptyList());
         r.setAlphabets(Collections.emptyList());
